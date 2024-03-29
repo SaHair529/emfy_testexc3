fdf188be07619d5cb48eb4ad06932baf3adce962
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2024-03-29 06:55:14 +0300

    Убрал headers

--- src/webhooks_handler.php
+++ src/webhooks_handler.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 use AmoCRM\Client\AmoCRMApiClient;
 use AmoCRM\Client\LongLivedAccessToken;
 use Dotenv\Dotenv;
