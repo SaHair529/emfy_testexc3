a64097324bb887ffe48d809e2a355baccc8df1a3
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2024-03-29 04:08:34 +0300

    CORS Policy

--- src/webhooks_handler.php
+++ src/webhooks_handler.php
@@ -1,8 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 use AmoCRM\Client\AmoCRMApiClient;
 use AmoCRM\Client\LongLivedAccessToken;
-use AmoCRM\Helpers\EntityTypesInterface;
 use Dotenv\Dotenv;
 
 require_once __DIR__.'/helper.php';
